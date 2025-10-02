b2790cb7fb10b8cf682a421899fb91f0a98a3846
KnapikQueenOfIceland <guilherme.knapik@gmail.com>
2025-10-02 13:43:46 -0300

    Update leitor.h

--- leitor.h
+++ leitor.h
@@ -1,3 +1,7 @@
+// Integrantes do grupo (ordem alfabética):
+// Guilherme Knapik - kingnapik
+// Nome do grupo no Canvas: RA2 3
+
 #ifndef LEITOR_H
 #define LEITOR_H
 
@@ -8,5 +12,6 @@ using namespace std;
 
 // Funcao para chamar linha p/ linha o arquivo
 vector<vector<string>> lerTokens(string arquivo);
+
 
 #endif
